f6ba39b97fcb8e1529941348dce050bcd7674f02
matakridha <mataharikridha@outlook.com>
2024-04-04 22:54:07 +0700

    add test execution

--- tests/E002-Negative.spec.ts
+++ tests/E002-Negative.spec.ts
@@ -1,27 +1,33 @@
 import { test, expect, Page } from '@playwright/test';
 import { textBox } from '../pages/steps/textBox-page';
 import { homePage } from '../pages/locator/home-page';
+import { formsPractice } from '../pages/steps/formsPractice';
+import { formsPage } from '../pages/locator/forms-page';
 
 const URL = 'https://demoqa.com';
-
-let TextBox: textBox;
-let HomePage: homePage;
-// Initialize elements in a function before using it
-export async function initializeElements(page: Page) {
-    TextBox = new textBox(page);
-    HomePage = new homePage(page);
-}
 
 test.describe('Negative Testing',() => {
     test.beforeEach(async ({page}) => {
         await page.goto(URL);
     })
-    test ('Input TextBox with null value', async ({page}) =>{
+    test ('TextBox - Input TextBox with null value', async ({page}) =>{
+        const HomePage = new homePage(page);
+        const TextBox = new textBox(page);
         await HomePage.goToElement();
         await TextBox.inputNothing();
     })
-    test ('Input TextBox with incorrect email format', async ({page}) =>{
+    test ('TextBox - Input TextBox with incorrect email format', async ({page}) =>{
+        const HomePage = new homePage(page);
+        const TextBox = new textBox(page);
         await HomePage.goToElement();
         await TextBox.inputIncorrectEmail();
+    })
+
+    test ('Form - Input no Mandatory', async ({page}) =>{
+        const HomePage = new homePage(page);
+        const FormsPractice = new formsPractice(page);
+        await HomePage.goToForm();
+        await FormsPractice.notInputMandatory();
+        await FormsPractice.verifyErrorMandatory();
     })
 });
