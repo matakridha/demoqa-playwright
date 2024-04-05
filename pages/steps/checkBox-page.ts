eb3d562fc2562017b11dfc0f48d5264e7dd258aa
matakridha <mataharikridha@outlook.com>
2024-04-05 22:30:35 +0700

    add new test scenario and error fixing

---
+++ pages/steps/checkBox-page.ts
@@ -0,0 +1,58 @@
+import { Page, Locator } from "playwright";
+import { ElementsPage } from "../locator/elements-page";
+import { expect } from "playwright/test";
+import { text } from "stream/consumers";
+
+export class checkBox {
+    readonly page:Page;
+    constructor(page:Page){this.page = page;}
+
+    async selectDesktop(){
+        const elements = new ElementsPage(this.page);
+        await elements.elementMenu.sCheckBox.isVisible();
+        await elements.elementMenu.sCheckBox.click();
+        await elements.checkBox.dropHome.click();
+        await elements.checkBox.dropDesktop.isVisible();
+        await elements.checkBox.checkDesktop.click();
+    }
+
+    async selectVeu(){
+        const elements = new ElementsPage(this.page);
+        await elements.checkBox.checkDesktop.click();
+        await elements.checkBox.dropDocuments.click();
+        await elements.checkBox.dropWorkspaces.click();
+        await elements.checkBox.checkVeu.click();
+    }
+
+    async selectMultiple(){
+        const elements = new ElementsPage(this.page);
+        await elements.checkBox.checkVeu.click();
+        await elements.checkBox.checkAngular.click();
+        await elements.checkBox.checkDownloads.click();
+    }
+
+    async verifySelectedDesktop(){
+        const text1 = await this.page.locator('//span[contains(text(), "desktop")]');
+        const text2 = await this.page.locator('//span[contains(text(), "notes")]');
+        const text3 = await this.page.locator('//span[contains(text(), "commands")]');
+        expect (await text1.isVisible()).toBe(true);
+        expect (await text2.isVisible()).toBe(true);
+        expect (await text3.isVisible()).toBe(true);
+    }
+    
+    async verifySelectedVeu(){
+        const text1 = await this.page.locator('//span[contains(text(), "veu")]')
+        expect (await text1.isVisible()).toBe(true);
+    }
+    async verifySelectedMulti(){
+        const text1 = await this.page.locator('//span[contains(text(), "angular")]')
+        const text2 = await this.page.locator('//span[contains(text(), "downloads")]')
+        const text3 = await this.page.locator('//span[contains(text(), "wordFile")]')
+        const text4 = await this.page.locator('//span[contains(text(), "excelFile")]')
+        expect (await text1.isVisible()).toBe(true);
+        expect (await text2.isVisible()).toBe(true);
+        expect (await text3.isVisible()).toBe(true);
+        expect (await text4.isVisible()).toBe(true);
+    }
+
+}
--- tests/E001-Positive.spec.ts
+++ tests/E001-Positive.spec.ts
@@ -1,37 +1,26 @@
 import { test, expect, Page } from '@playwright/test';
 import { textBox } from '../pages/steps/textBox-page';
-import { elementsPage } from '../pages/locator/elements-page';
+import { ElementsPage } from '../pages/locator/elements-page';
 import { homePage } from '../pages/locator/home-page';
 import { formsPractice } from '../pages/steps/formsPractice';
 import { formsPage } from '../pages/locator/forms-page';
+import { checkBox } from '../pages/steps/checkBox-page';
+import { radioButton } from '../pages/steps/radioButton-page';
 
 const URL = 'https://demoqa.com';
-/*
-let TextBox: textBox;
-let HomePage: homePage;
-let FormsPractice: formsPractice;
-let FormsPage: formsPage;
-// Initialize elements in a function before using it
 
-export async function initializeElements(page: Page) {
-    TextBox = new textBox(page);
-    HomePage = new homePage(page);
-    FormsPractice = new formsPractice(page);
-    FormsPage = new formsPage(page);
-}
-*/
 test.describe('Positive Testing',() => {
     test.beforeEach(async ({page}) => {
         await page.goto(URL);
         await expect(page).toHaveURL(URL);
     })
-    test ('Input TextBox with correct value', async ({page}) =>{
+    test ('text box - Input TextBox with correct value', async ({page}) =>{
         const HomePage = new homePage(page);
         const TextBox = new textBox(page);
         await HomePage.goToElement();
         await TextBox.inputCorrectValue();
     })
-    test ('Input Form with valid value', async ({page}) =>{
+    test ('form - Input Form with valid value', async ({page}) =>{
         const HomePage = new homePage(page);
         const FormsPractice = new formsPractice(page);
         const FormsPage = new formsPage(page);
@@ -39,6 +28,24 @@ test.describe('Positive Testing',() => {
         await FormsPractice.inputValidForm();
         await FormsPage.btnSubmit.click();
         await FormsPractice.verifyFormValue();
+    })
+    test ('check box - function', async ({page}) => {
+        const HomePage = new homePage(page);
+        const CheckBox = new checkBox(page);
+        await HomePage.goToElement();
+        await CheckBox.selectDesktop();
+        await CheckBox.verifySelectedDesktop();
+        await CheckBox.selectVeu();
+        await CheckBox.verifySelectedVeu();
+        await CheckBox.selectMultiple();
+        await CheckBox.verifySelectedMulti();
+    })
+    test ('radio box - test function', async ({page}) => {
+        const HomePage = new homePage(page);
+        const RadioButton = new radioButton(page);
+        await HomePage.goToElement();
+        await RadioButton.selectYes();
+        await RadioButton.verifyRadioYes();
     })
 });
 
